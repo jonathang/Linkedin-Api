d23db5b5989902aef11e756235ab9cfddd9c45a0
Peyton Sherwood <peytons@gmail.com>
2014-03-13 22:23:06 -0400

    oh yeah.. re works

--- app/lib/rulemaker.js
+++ app/lib/rulemaker.js
@@ -1,7 +1,8 @@
 var url = require('url'),
     qs = require('querystring'),
     util = require('util'),
-    users = require('./users');
+    users = require('./users'),
+    RuleEngine = require('./business-rules/lib/business-rules/rule-engine');
 
 
 function rules(req, res) {
@@ -10,7 +11,28 @@ function rules(req, res) {
 
 function rulesendpoint(req, res) {
     console.log(util.inspect(req.body.rules.all));
-    res.send("OK");
+    //res.send("OK");
+    re = new RuleEngine({
+        conditions: req.body.rules,
+        actions: [{name: "action-select", value: "giveDrink", fields: [{name: "drinkType", value: "martini"}]}]
+    });
+
+    var record = {
+        firstname: "Peyton",
+        age:       "32"
+    }
+    var conditionsAdapter = {
+        firstnameField: record.firstname,
+        ageField: record.age
+    };
+    var actionsAdapter = {
+        giveDrink: function(data) {
+            var thedata = util.inspect(record);
+            console.log("data is ...." + thedata);
+            res.send("some data: " + thedata + " ....");
+        }
+    }
+    re.run(conditionsAdapter, actionsAdapter);
 }
 
 exports.rules = rules;
